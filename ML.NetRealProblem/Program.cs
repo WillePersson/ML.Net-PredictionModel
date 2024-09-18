c484ba7ff57aa174edafa0a9857a8f7aa0587269
wille <144245981+WillePersson@users.noreply.github.com>
2024-09-18 09:10:31 +0200

    Add project files.

---
+++ ML.NetRealProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using ML_NetRealProblem;
+
+namespace ML.NetRealProblem
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Load sample data
+			var sampleData = new MachineFailureModel.ModelInput()
+			{
+				Product_ID = @"L47181",
+				Type = @"L",
+				Air_temperature = 298.2F,
+				Process_temperature = 308.7F,
+				Rotational_speed = 1408F,
+				Torque = 46.3F,
+				Tool_wear = 3F,
+			};
+
+			//Load model and predict output
+			var result = MachineFailureModel.Predict(sampleData);
+		}
+	}
+}
